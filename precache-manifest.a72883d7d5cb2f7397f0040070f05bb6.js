27764b3232c85df1e5b112ce8fdc0afde8148dae
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2020-07-16 04:22:25 +0530

    Updates

---
+++ precache-manifest.a72883d7d5cb2f7397f0040070f05bb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3565521eaa5d5a0c66573030ca1ee99",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "81e082ce6f4566249903",
+    "url": "/react-tic-tac-toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "339d758a833bec262c0b",
+    "url": "/react-tic-tac-toe/static/css/main.b709c0bd.chunk.css"
+  },
+  {
+    "revision": "81e082ce6f4566249903",
+    "url": "/react-tic-tac-toe/static/js/2.378c1b80.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-tic-tac-toe/static/js/2.378c1b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339d758a833bec262c0b",
+    "url": "/react-tic-tac-toe/static/js/main.7f9d81f7.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  }
+]);
